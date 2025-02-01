79abc80592454dc3c7c98c16cd57ecf08a607cd4
ashishpatelengineering <114289015+ashishpatelengineering@users.noreply.github.com>
2025-02-01 06:01:48 +0530

    Update rag_utility.py

--- rag_utility.py
+++ rag_utility.py
@@ -1,6 +1,11 @@
 import os
 import json
 
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
+
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
